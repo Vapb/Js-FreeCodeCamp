01749b487cd9972c3cb74933088d0803e58c074b
vapb <vapb7123@gmail.com>
2022-03-25 20:48:46 -0300

    Add Debugging.js

---
+++ Debugging.js
@@ -0,0 +1,32 @@
+// Use the JavaScript Console to Check the Value of a Variable
+
+let a = 5;
+console.log(a);
+
+// Understanding the Differences between the freeCodeCamp and Browser Console
+
+console.log(a);
+console.clear();
+
+// Use typeof to Check the Type of a Variable
+
+let seven = 7;
+let three = "3";
+console.log(typeof seven);
+console.log(typeof three);
+
+// Catch Misspelled Variable and Function Names
+// Catch Unclosed Parentheses, Brackets, Braces and Quotes
+
+// Catch Mixed Usage of Single and Double Quotes
+
+let innerHtml1 = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+let innerHtml2 = '<p>Click here to <a href="#Home">return home</a></p>';
+
+// Catch Use of Assignment Operator Instead of Equality Operator
+// Catch Missing Open and Closing Parenthesis After a Function Call
+// Catch Arguments Passed in the Wrong Order When Calling a Function
+// Catch Off By One Errors When Using Indexing
+// Use Caution When Reinitializing Variables Inside a Loop
+// Prevent Infinite Loops with a Valid Terminal Condition
+
